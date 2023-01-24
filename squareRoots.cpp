35bf90ec080ca640a827f2338b688abc3e557f16
Ansh Singh Sonkhia <anshssonkhia@gmail.com>
2023-01-24 20:22:03 +0530

    square root

---
+++ squareRoots.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    double x;
+
+    cout << "Input number: " << endl;
+    cin >> x;
+
+    double sqrt_x = sqrt(x);
+
+    cout << "Sq. Root of " << x;
+    cout << " is: " << sqrt_x << endl;
+}
